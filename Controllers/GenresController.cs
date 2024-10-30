2b6d8f22bedd128924749189be820c6588463193
Ventura <ventura.py3@gmail.com>
2024-10-30 17:01:26 -0300

    feat: Add connection to database

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,32 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class GenresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre
+                {
+                    Id = 1,
+                    Name = "Romance"
+                },
+                new Genre
+                {
+                    Id = 2,
+                    Name = "Ficção Científica"
+                },
+                new Genre
+                {
+                    Id = 3,
+                    Name = "Comédia"
+                }
+            };
+
+            return View(genres);
+        }
+    }
+}
