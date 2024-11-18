dec19a291bad0745d81345e8e7efbf183d69a4c6
Luigi <luigicumaru@gmail.com>
2024-11-18 16:59:31 -0300

    feat: create Book model

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -9,7 +9,7 @@ namespace Bookstore.Models
         [Display(Name = "Nome")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Name { get; set; }
-        //public ICollection<Book> Books { get; set; } = new List<Book>();
+        public ICollection<Book> Books { get; set; } = new List<Book>();
 
         public Genre()
         {
