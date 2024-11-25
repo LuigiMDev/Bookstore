3ee1df21118d9e80a9b915f0c09a463b6c875b7c
Luigi <luigicumaru@gmail.com>
2024-11-25 17:14:04 -0300

    feat: init BooksController

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bookstore.Data;
+using Bookstore.Models;
+using Bookstore.Services;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Bookstore.Models.ViewModels;
+using System.Diagnostics;
+
+namespace Bookstore.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookService _service;
+
+        public BooksController(BookService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _service.FindAllAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "id não fornecido" });
+            }
+            Book book = await _service.Details(id.Value);
+            if (book is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não foi encontrado" });
+            }
+
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            await _service.Create(book);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+
+            if (id is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "id não fornecido" });
+            }
+            Book book = await _service.FindById(id.Value);
+            if (book is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não foi encontrado" });
+            }
+
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Book book, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            book.Id = id;
+            await _service.Edit(book);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "id não fornecido" });
+            }
+            Book book = await _service.FindById(id.Value);
+            if (book is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não foi encontrado" });
+            }
+
+            await _service.Delete(id.Value);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Services/BookService.cs
@@ -0,0 +1,75 @@
+﻿using Bookstore.Data;
+using Bookstore.Models;
+using Bookstore.Services.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Services
+{
+    public class BookService
+    {
+        private readonly BookstoreContext _context;
+
+        public BookService(BookstoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Book>> FindAllAsync()
+        {
+            return await _context.Books.Include(x => x.Genres).ToListAsync();
+        }
+
+        public async Task Create(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            try
+            {
+                Book book = await _context.Books.FirstOrDefaultAsync(book => book.Id == id);
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new IntegrityException(ex.Message);
+            }
+
+        }
+
+        public async Task Edit(Book bookEdited)
+        {
+            bool hasAny = await _context.Books.AnyAsync(x => x.Id == bookEdited.Id);
+
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id não encontrado");
+            }
+
+            try
+            {
+                _context.Books.Update(bookEdited);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new DbConcorrencyException(ex.Message);
+            }
+
+        }
+
+        public async Task<Book> FindById(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task<Book> Details(int id)
+        {
+            return await _context.Books.Include(gen => gen.Genres).FirstOrDefaultAsync(gen => gen.Id == id);
+        }
+    }
+}
